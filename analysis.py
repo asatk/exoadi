57020451288345f9987eb7de6c4f069c529523d1
Anthony Atkinson <ep1cantw0n@gmail.com>
2023-06-20 13:33:48 +0100

    zscale plots

--- analysis.py
+++ analysis.py
@@ -1,58 +1,78 @@
 from astropy.io import fits
 from astropy.modeling import models, fitting
+from astropy.visualization import ZScaleInterval
 from matplotlib import pyplot as plt
 import multiprocessing as mp
 import numpy as np
 
-datafilepath = "./out/data_mean_00040_00080.fits"
-outfilepath = "./out/signal_plot.png"
+def plot_redux(zscale=False):
 
-with fits.open(datafilepath) as datahdu:
-    data = np.array(datahdu[1].data)
+    datafilepath = "./out/data_mean_00040_00080.fits"
+    outfilepath = "./out/signal_mean_plot.png"
 
-xdim = 63
-ydim = 63
-yind, xind = np.mgrid[0:xdim, 0:ydim]
-subdata = data[35:60, 0:25]
+    with fits.open(datafilepath) as datahdu:
+        data = np.array(datahdu[1].data)
 
-init_amplitude = np.max(subdata)
-init_x_mean = 10
-init_y_mean = 40
-init_cov_matrix = 5 * np.eye(2)
-init_gaus = models.Gaussian2D(amplitude=init_amplitude, x_mean=init_x_mean, y_mean=init_y_mean, cov_matrix=init_cov_matrix)
-fitter = fitting.LevMarLSQFitter()
+    xdim = 63
+    ydim = 63
+    yind, xind = np.mgrid[0:xdim, 0:ydim]
+    subdata = data[35:60, 0:25]
 
-gausfit = fitter(init_gaus, xind, yind, data)
-params = [gausfit.amplitude.value, gausfit.x_mean.value, gausfit.y_mean.value, gausfit.x_stddev.value, gausfit.y_stddev.value]
-print(params)
+    init_amplitude = np.max(subdata)
+    init_x_mean = 10
+    init_y_mean = 40
+    init_cov_matrix = 5 * np.eye(2)
+    init_gaus = models.Gaussian2D(amplitude=init_amplitude, x_mean=init_x_mean, y_mean=init_y_mean, cov_matrix=init_cov_matrix)
+    fitter = fitting.LevMarLSQFitter()
 
-bkgd_mean = np.mean(data)
-bkgd_sdev = np.std(data)
-print(bkgd_mean, bkgd_sdev, bkgd_mean + 5 * bkgd_sdev, (params[0] - bkgd_mean) / bkgd_sdev)
+    gausfit = fitter(init_gaus, xind, yind, data)
+    params = [gausfit.amplitude.value, gausfit.x_mean.value, gausfit.y_mean.value, gausfit.x_stddev.value, gausfit.y_stddev.value]
+    # print(params)
 
-fig = plt.figure(figsize=(15, 5), facecolor='white')
-ax1 = fig.add_subplot(131)
-ax2 = fig.add_subplot(132)
-ax3 = fig.add_subplot(133)
+    bkgd_mean = np.mean(data)
+    bkgd_sdev = np.std(data)
+    # print(bkgd_mean, bkgd_sdev, bkgd_mean + 5 * bkgd_sdev, (params[0] - bkgd_mean) / bkgd_sdev)
 
-ax1.set_title("cADI image after combining\nby mean over $\lambda$ and $t$")
-im1 = ax1.imshow(data)
-fig.colorbar(im1, ax=ax1)
+    if zscale:
+        zmin, zmax = ZScaleInterval().get_limits(data)
+    else:
+        zmin = np.min(data)
+        zmax = np.max(data)
 
-datastr = r'$\mu_x = ' + '%.03f$\n'%(params[1]) + r'$\sigma_x = ' + '%.05f$\n'%(params[3]) + \
-          r'$\mu_y = ' + '%.03f$\n'%(params[2]) + r'$\sigma_y = ' + '%.05f$\n'%(params[4]) + \
-          r'amp = ' + '$%.03f$\n'%(params[0]) + r'$\mu_{bkgd} = ' + '%.05f$\n'%(bkgd_mean) + \
-          r'$\sigma_{bkgd} = ' + '%.05f$'%(bkgd_sdev)
-ax2.text(0.6, 0.5, datastr, fontsize=12, transform=ax2.transAxes,
-         bbox=dict(facecolor='#f5f5dc', alpha=0.5))
-ax2.set_title("2D Gaussian fit to planet signal")
-im2 = ax2.imshow(gausfit(xind, yind))
-fig.colorbar(im2, ax=ax2)
+    # --- FIG --- #
+    fig = plt.figure(figsize=(15, 5), facecolor='white')
+    ax1 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(133)
 
-ax3.set_title("Residual image")
-im3 = ax3.imshow(data - gausfit(xind, yind))
-fig.colorbar(im3, ax=ax3)
+    # --- PLOT 1 --- #
+    ax1.set_title("cADI image after combining\nby mean over $\lambda$ and $t$")
+    ax1_data = data
+    im1 = ax1.imshow(ax1_data, vmin=zmin, vmax=zmax)
+    fig.colorbar(im1, ax=ax1)
 
-fig.tight_layout()
-fig.savefig(outfilepath)
-plt.show()
+    # --- PLOT 2 --- #
+    datastr = r'$\mu_x = ' + '%.03f$\n'%(params[1]) + r'$\sigma_x = ' + '%.05f$\n'%(params[3]) + \
+            r'$\mu_y = ' + '%.03f$\n'%(params[2]) + r'$\sigma_y = ' + '%.05f$\n'%(params[4]) + \
+            r'amp = ' + '$%.03f$\n'%(params[0]) + r'$\mu_{bkgd} = ' + '%.05f$\n'%(bkgd_mean) + \
+            r'$\sigma_{bkgd} = ' + '%.05f$'%(bkgd_sdev)
+    ax2.text(0.6, 0.5, datastr, fontsize=12, transform=ax2.transAxes,
+            bbox=dict(facecolor='#f5f5dc', alpha=0.5))
+    ax2.set_title("2D Gaussian fit to planet signal")
+    ax2_data = gausfit(xind, yind)
+    im2 = ax2.imshow(ax2_data, vmin=zmin, vmax=zmax)
+    fig.colorbar(im2, ax=ax2)
+
+    # --- PLOT 3 --- #
+    ax3.set_title("Residual image")
+    ax3_data = data - gausfit(xind, yind)
+    im3 = ax3.imshow(ax3_data, vmin=zmin, vmax=zmax)
+    fig.colorbar(im3, ax=ax3)
+
+    # --- FIG --- #
+    fig.tight_layout()
+    fig.savefig(outfilepath)
+    plt.show()
+
+if __name__ == "__main__":
+    plot_redux(zscale=False)
--- redux.py
+++ redux.py
@@ -19,7 +19,7 @@ datafilepath = datadirpath + datafilename
 datafilepaths = [datafilepath] * nchnls
 
 outdirname = "out/"
-outchannelfilename = "data_%05i_median.fits"
+outchannelfilename = "data_%05i_mean.fits"
 outallfilename = "data_mean_%05i_%05i.fits"%(firstdatafilenum, lastdatafilenum)
 
 # outchannelfilepath = outdirname + outchannelfilename
@@ -28,5 +28,33 @@ outchannelfilepaths = [outchannelfilepath] * nchnls
 outallfilepath = outdirname + outallfilename
 # --- PATHS --- #
 
-redux_utils.combine_channels(datafilenums, datafilepaths, combine_fn=np.mean, channel_combine_fn=np.mean, outfilepath=outallfilepath, outchannelfilepaths=outchannelfilepaths)
+if __name__ == "__main__":
+
+    # # --- DATA INFO --- #
+    # # datafilenum = 52
+    # xdim = 63
+    # ydim = 63
+    # firstdatafilenum = 40
+    # lastdatafilenum = 80
+    # datafilenums = list(range(firstdatafilenum, lastdatafilenum))
+    # nchnls = len(datafilenums)
+    # # --- DATA INFO --- #
+
+    # # --- PATHS --- #
+    # datadirpath = "./data/005_center_multishift/"
+    # datafilename = "wl_channel_%05i.fits"
+    # datafilepath = datadirpath + datafilename
+    # datafilepaths = [datafilepath] * nchnls
+
+    # outdirname = "out/"
+    # outchannelfilename = "data_%05i_median.fits"
+    # outallfilename = "data_mean_%05i_%05i.fits"%(firstdatafilenum, lastdatafilenum)
+
+    # # outchannelfilepath = outdirname + outchannelfilename
+    # outchannelfilepath = None
+    # outchannelfilepaths = [outchannelfilepath] * nchnls
+    # outallfilepath = outdirname + outallfilename
+    # # --- PATHS --- #
+
+    redux_utils.combine_channels(datafilenums, datafilepaths, combine_fn=np.mean, channel_combine_fn=np.mean, outfilepath=outallfilepath, outchannelfilepaths=outchannelfilepaths)
 
