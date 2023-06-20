43dff2333448204f2beeeec2fa3cd818127b5490
Anthony Atkinson <ep1cantw0n@gmail.com>
2023-06-20 12:40:26 +0100

    modularity

--- analysis.py
+++ analysis.py
@@ -5,12 +5,54 @@ import multiprocessing as mp
 import numpy as np
 
 datafilepath = "./out/data_mean_00040_00080.fits"
+outfilepath = "./out/signal_plot.png"
 
 with fits.open(datafilepath) as datahdu:
-    data = datahdu[0].data
+    data = np.array(datahdu[1].data)
 
 xdim = 63
 ydim = 63
-subdata = data[0:xdim/2, 0:ydim/2]
+yind, xind = np.mgrid[0:xdim, 0:ydim]
+subdata = data[35:60, 0:25]
 
-gausmodel = models.Gaussian2D(amplitude=np.max(subdata), x_mean=xdim/4, y_mean=ydim/4)
+init_amplitude = np.max(subdata)
+init_x_mean = 10
+init_y_mean = 40
+init_cov_matrix = 5 * np.eye(2)
+init_gaus = models.Gaussian2D(amplitude=init_amplitude, x_mean=init_x_mean, y_mean=init_y_mean, cov_matrix=init_cov_matrix)
+fitter = fitting.LevMarLSQFitter()
+
+gausfit = fitter(init_gaus, xind, yind, data)
+params = [gausfit.amplitude.value, gausfit.x_mean.value, gausfit.y_mean.value, gausfit.x_stddev.value, gausfit.y_stddev.value]
+print(params)
+
+bkgd_mean = np.mean(data)
+bkgd_sdev = np.std(data)
+print(bkgd_mean, bkgd_sdev, bkgd_mean + 5 * bkgd_sdev, (params[0] - bkgd_mean) / bkgd_sdev)
+
+fig = plt.figure(figsize=(15, 5), facecolor='white')
+ax1 = fig.add_subplot(131)
+ax2 = fig.add_subplot(132)
+ax3 = fig.add_subplot(133)
+
+ax1.set_title("cADI image after combining\nby mean over $\lambda$ and $t$")
+im1 = ax1.imshow(data)
+fig.colorbar(im1, ax=ax1)
+
+datastr = r'$\mu_x = ' + '%.03f$\n'%(params[1]) + r'$\sigma_x = ' + '%.05f$\n'%(params[3]) + \
+          r'$\mu_y = ' + '%.03f$\n'%(params[2]) + r'$\sigma_y = ' + '%.05f$\n'%(params[4]) + \
+          r'amp = ' + '$%.03f$\n'%(params[0]) + r'$\mu_{bkgd} = ' + '%.05f$\n'%(bkgd_mean) + \
+          r'$\sigma_{bkgd} = ' + '%.05f$'%(bkgd_sdev)
+ax2.text(0.6, 0.5, datastr, fontsize=12, transform=ax2.transAxes,
+         bbox=dict(facecolor='#f5f5dc', alpha=0.5))
+ax2.set_title("2D Gaussian fit to planet signal")
+im2 = ax2.imshow(gausfit(xind, yind))
+fig.colorbar(im2, ax=ax2)
+
+ax3.set_title("Residual image")
+im3 = ax3.imshow(data - gausfit(xind, yind))
+fig.colorbar(im3, ax=ax3)
+
+fig.tight_layout()
+fig.savefig(outfilepath)
+plt.show()
---
+++ redux_utils.py
@@ -0,0 +1,67 @@
+from astropy.io import ascii
+from astropy.io import fits
+import multiprocessing as mp
+import numpy as np
+from scipy import ndimage
+from typing import Callable
+
+# get list of angles for de-rotation
+anglesfilepath = "data/parangs_bads_removed.txt"
+anglestable = ascii.read(anglesfilepath, format="no_header", data_start=0)
+angles = anglestable['col1'].data
+
+# save array to specified path
+def savedata(data: np.ndarray, path: str, overwrite: bool=True) -> None:
+    hdul = fits.HDUList()
+    hdul.append(fits.PrimaryHDU())
+    hdul.append(fits.ImageHDU(data=data))
+    hdul.writeto(path, overwrite=overwrite)
+
+def reduce_channel(datafilenum: int, datafilepath: str, combine_fn: Callable[[np.ndarray], np.ndarray]=np.mean, outfilepath: str=None) -> np.ndarray:
+
+    # load data cube
+    with fits.open(datafilepath%(datafilenum)) as datahdu:
+        data = datahdu[0].data
+        assert len(angles) == len(data)
+
+    # subtract median PSF from all frames in this wavelength channel
+    medpsf = np.median(data, axis=0)
+    datasub = np.subtract(data, medpsf)
+
+    # de-rotate the data
+    datarot = np.empty_like(datasub)
+    for i in range(len(angles)):
+        datarot[i] = ndimage.rotate(datasub[i], -1 * angles[i], reshape=False)
+
+    # combine de-rotated images in this wavelength channel
+    datacombined = combine_fn(datarot, axis=0)
+
+    if outfilepath is not None:
+        savedata(datacombined, outfilepath%(datafilenum))
+
+    return datacombined
+
+# combine each wavelength channel
+def combine_channels(datafilenums: list[int], datafilepaths: list[str], combine_fn: Callable[[np.ndarray], np.ndarray]=np.mean, channel_combine_fn: Callable[[np.ndarray], np.ndarray]=np.mean, outfilepath: str=None, outchannelfilepaths: list[str]=None) -> np.ndarray:
+
+    nchnls = len(datafilenums)
+    assert nchnls == len(datafilepaths)
+
+    if outchannelfilepaths is not None:
+        assert nchnls == len(outchannelfilepaths)
+    else:
+        outchannelfilepaths = list[None] * nchnls
+
+    # calculate ADI image for each wavelength channel
+    with mp.Pool(8) as pool:
+        channels = np.array(pool.starmap(
+            reduce_channel,
+            zip(datafilenums, datafilepaths, [channel_combine_fn] * nchnls, outchannelfilepaths)))
+
+    # combine ADI images across channels
+    redux = combine_fn(channels, axis=0)
+    
+    if outfilepath is not None:
+        savedata(redux, outfilepath)
+
+    return redux
